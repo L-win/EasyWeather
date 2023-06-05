77b1bdf91a3efdf4c3aa18f6b71b2d8f2329b5ff
Elvin Agha <33497735+L-win@users.noreply.github.com>
2023-06-06 00:19:12 +0400

    Initial commit.

---
+++ app/src/main/java/com/elvina/easyweather/MainActivity.kt
@@ -0,0 +1,30 @@
+package com.elvina.easyweather
+
+import android.os.AsyncTask
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.widget.ProgressBar
+import android.widget.RelativeLayout
+
+class MainActivity : AppCompatActivity() {
+
+    val LOCATION:String = "Tbilisi, Georgia"
+    val API:String = ""
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        fetchWeatherTask.execute()
+
+    }
+    inner class fetchWeatherTask() : AsyncTask<String, Void, String>()
+    {
+        override fun onPreExecute() {
+            super.onPreExecute()
+            findViewById<ProgressBar>(R.id.Progressbar).visibility = View.VISIBLE
+            findViewById<RelativeLayout>(R.id.MainContainer).visibility = View.VISIBLE
+        }
+    }
+}
