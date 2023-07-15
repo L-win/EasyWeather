0f5d8c6401885a2664ef8e485ced11febb36e275
Elvin Agha <33497735+L-win@users.noreply.github.com>
2023-07-15 23:50:54 +0400

    Parse json in seperate class.

--- app/src/main/java/com/elvina/easyweather/MainActivity.kt
+++ app/src/main/java/com/elvina/easyweather/MainActivity.kt
@@ -9,9 +9,6 @@ import android.widget.RelativeLayout
 import android.widget.TextView
 import org.json.JSONObject
 import java.net.URL
-import java.text.SimpleDateFormat
-import java.util.Date
-
 
 class MainActivity : AppCompatActivity() {
 
@@ -22,10 +19,12 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        println("CONSOLE: START")
         if (!API.isEmpty()) {
             fetchWeatherTask().execute()
         } else {
+            findViewById<RelativeLayout>(R.id.main_container).visibility = View.GONE
+            findViewById<TextView>(R.id.error_message).visibility = View.VISIBLE
+            findViewById<TextView>(R.id.error_message).text = "No API key."
             println("CONSOLE: NO API KEY")
         }
 
@@ -40,13 +39,10 @@ class MainActivity : AppCompatActivity() {
         }
 
         override fun doInBackground(vararg params: String?): String? {
-            println("CONSOLE: doInBackground")
             var response: String?
             try {
-                println("CONSOLE: doInBackground Try")
                 response = URL("https://api.weatherapi.com/v1/forecast.json?key=$API&q=$LOCATION")
                     .readText(Charsets.UTF_8)
-                println("CONSOLE: doInBackground Try 2")
             } catch (e: Exception) {
                 println("CONSOLE: doInBackground Catch" + e.message)
                 response = null
@@ -58,36 +54,25 @@ class MainActivity : AppCompatActivity() {
             super.onPostExecute(result)
             try {
                 val json = JSONObject(result)
+                val data = ParseJson(json)
 
-                val location = json.getJSONObject("location").getString("name")
-                val dataCurrent = json.getJSONObject("current")
-
-                val weatherType = dataCurrent.getJSONObject("condition").getString("text")
-                val weatherTemp = dataCurrent.getInt("temp_c")
-
-                val oldDate = SimpleDateFormat("yyyy-MM-dd HH:mm").parse(dataCurrent.getString("last_updated"))
-                val currentDate = Date()
-                val diff = currentDate.getTime() - oldDate.getTime()
-                val diffM = (diff / 1000) / 60
-
-                val dataForecast =
-                    json.getJSONObject("forecast").getJSONArray("forecastday").getJSONObject(0)
-                val dayForecast = dataForecast.getJSONObject("day")
-
-                val hourForecast = dataForecast.getJSONArray("hour").getJSONObject(15)
-//                println("CONSOLE: "+hourForecast.getString("temp_c")+" "+hourForecast.getString("time"))
+                val location = data.locationName()
+                val weatherType = data.currentWeatherType()
+                val weatherTemp = data.currentWeatherTemp()
+                val lastUpdatedTime = data.lastUpdatedTime()
+                val dayForecast = data.forecastToday()
+//                val hourForecast = data.forecastHourly().getJSONObject(15)
+//                println("CONSOLE: " + hourForecast.getString("temp_c") + " " + hourForecast.getString("time"))
 
                 findViewById<TextView>(R.id.location).text = location
-                findViewById<TextView>(R.id.weather_temperature).text = weatherTemp.toString() + " °"
+                findViewById<TextView>(R.id.weather_temperature).text = weatherTemp + " °"
                 findViewById<TextView>(R.id.weather_type).text = weatherType
-                findViewById<TextView>(R.id.update_time).text = diffM.toString() + "m ago"
+                findViewById<TextView>(R.id.update_time).text = lastUpdatedTime + "m ago"
                 findViewById<TextView>(R.id.weather_temp_min).text = "MIN " + dayForecast.getString("mintemp_c")
                 findViewById<TextView>(R.id.weather_temp_max).text = "MAX " + dayForecast.getString("maxtemp_c")
 
                 findViewById<ProgressBar>(R.id.progressbar).visibility = View.GONE
                 findViewById<RelativeLayout>(R.id.main_container).visibility = View.VISIBLE
-
-                println("CONSOLE: END")
             } catch (e: Exception) {
                 findViewById<ProgressBar>(R.id.progressbar).visibility = View.GONE
                 findViewById<TextView>(R.id.error_message).visibility = View.VISIBLE
--- app/src/main/java/com/elvina/easyweather/ParseJson.kt
+++ app/src/main/java/com/elvina/easyweather/ParseJson.kt
@@ -1,6 +1,42 @@
 package com.elvina.easyweather
 
+import org.json.JSONArray
 import org.json.JSONObject
+import java.text.SimpleDateFormat
+import java.util.Date
 
 class ParseJson(val json: JSONObject) {
+
+    fun locationName(): String {
+        return json.getJSONObject("location").getString("name")
+    }
+
+    fun currentWeatherType(): String {
+        return json.getJSONObject("current").getJSONObject("condition").getString("text")
+//        val weatherType = dataCurrent.getJSONObject("condition").getString("text")
+    }
+
+    fun currentWeatherTemp(): String {
+        return json.getJSONObject("current").getInt("temp_c").toString()
+    }
+
+    fun lastUpdatedTime(): String {
+        val oldDate =
+            SimpleDateFormat("yyyy-MM-dd HH:mm").parse(json.getJSONObject("current").getString("last_updated"))
+        val currentDate = Date()
+        val diff = currentDate.getTime() - oldDate.getTime()
+        val diffM = (diff / 1000) / 60
+        return diffM.toString()
+    }
+
+    fun forecastToday(): JSONObject {
+        val forecast = json.getJSONObject("forecast").getJSONArray("forecastday").getJSONObject(0)
+        return forecast.getJSONObject("day")
+    }
+
+    fun forecastHourly(): JSONArray {
+        val forecast =
+            json.getJSONObject("forecast").getJSONArray("forecastday").getJSONObject(0)
+        return forecast.getJSONArray("hour")
+    }
 }
